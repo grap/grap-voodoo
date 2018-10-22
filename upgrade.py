a683854f5223186927cdb71caf101fc54689cad1
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-10-22 10:37:07 +0200

    wip

--- upgrade.py
+++ upgrade.py
@@ -5,8 +5,11 @@ The purpose is both to cover the most common use-case (updating all modules)
 and to provide an example of how this works.
 """
 
+import imp
+import os
+
+
 from anybox.recipe.odoo.runtime.session import OpenERPVersion
-import os, imp
 
 
 def run(session, logger):
@@ -20,13 +23,12 @@ def run(session, logger):
     files = [f.replace('.py', '') for f in os.listdir('upgrade')
              if f[-3:] == '.py']
     files.sort(key=OpenERPVersion)
-    if 'current' in files:
-        files.remove('current')
-        files.append('current')
-        for version in files:
-            if session.db_version < version or version == 'current':
-                script = imp.load_source('script', 'upgrade/%s.py' % version)
-                script.run(session, logger)
-                if version != 'current':
-                    session.db_version = version
-                session.cr.commit()
+    for version in files:
+        if session.db_version < version or version == 'always':
+            file_name = 'upgrade/%s.py' % version
+            logger.info("UPGRADE : Running upgrade file %s" % file_name)
+            script = imp.load_source('script', file_name)
+            script.run(session, logger)
+            if version != 'always':
+                session.db_version = version
+            session.cr.commit()
---
+++ upgrade/always.py
@@ -0,0 +1,93 @@
+# coding: utf-8
+from openerp.api import Environment
+
+
+INSTALL = [
+    'product_analytic',
+    'account_invoice_pricelist_stock_account',
+    'multi_search_product',
+    'multi_search_partner',
+    'product_category_product_qty',
+]
+
+UPDATE = [
+    'account_invoice_pricelist',
+    'product_margin_classification',
+    'product_standard_price_tax_included',
+    'stock_picking_quick_edit',
+    'stock_picking_mass_change',
+    'pos_invoicing',
+    'account_export_ebp',
+    'product_categ_search_complete_name',
+    'account_move_change_number',
+    'invoice_verified_state',
+]
+
+
+def uninstall(session, modules):
+    env = Environment(session.cr, 1, {})
+    module_obj = env['ir.module.module']
+    modules = module_obj.search([('name', 'in', modules)])
+    modules.module_uninstall()
+
+
+def handle_field_renaming(env, logger, model_name, old_name, new_name):
+    model = env['ir.model'].search([('model', '=', model_name)])[0]
+    tiles = env['tile.tile'].search([
+        ('model_id', '=', model.id),
+        '|', ('domain', 'ilike', "'%s'" % old_name),
+        ('domain', 'ilike', '"%s"' % old_name)])
+    if tiles:
+        logger.info("UPGRADE: change domain of %s for %d tiles" % (
+            model_name, len(tiles)))
+        for tile in tiles:
+            tile.domain = tile.domain.replace(
+                "'%s'" % (old_name), "'%s'" % (new_name)).replace(
+                    '"%s"' % (old_name), '"%s"' % (new_name))
+
+    filters = env['ir.filters'].search([
+        ('model_id', '=', model_name),
+        '|', ('domain', 'ilike', "'%s'" % old_name),
+        ('domain', 'ilike', '"%s"' % old_name)])
+    if filters:
+        logger.info("UPGRADE: change domain of %s for %d filters" % (
+            model_name, len(filters)))
+        for filter in filters:
+            filter.domain = filter.domain.replace(
+                "'%s'" % (old_name), "'%s'" % (new_name)).replace(
+                    '"%s"' % (old_name), '"%s"' % (new_name))
+
+
+def run(session, logger):
+    # if INSTALL:
+    #     session.install_modules(INSTALL)
+    # if UPDATE:
+    #     session.update_modules(UPDATE)
+
+    # uninstall(session, ['account_invoice_pricelist_sale_stock'])
+    # uninstall(session, ['product_improved_search'])
+    # uninstall(session, ['product_category_improve'])
+
+    # Extra Operation
+    env = Environment(session.cr, 1, {})
+
+    # refactoring of pos_invoicing
+    # handle_field_renaming(
+    #     env, logger, 'account.invoice', 'forbid_payment',
+    #     'pos_pending_payment')
+
+    # refactoring of account_export_ebp
+    # handle_field_renaming(
+    #     env, logger, 'account.move', 'exported_ebp_id',
+    #     'ebp_export_id')
+
+    # Uninstall Italian language
+    # lang = env['res.lang'].search([('code', '=', 'it_IT')])
+    # lang.active = False
+    # lang.unlink()
+
+    # <TODO>
+    # configure :  multi_search_product
+    # configure :  multi_search_partner
+    # Call clean database
+    # </TODO>
